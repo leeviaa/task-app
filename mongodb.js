dc0626028ac4e187bd74b7d9a0fd893db56ec2d1
leeviaa <leeviaa22@gmail.com>
2020-08-01 09:43:09 -0400

    initial commit

---
+++ mongodb.js
@@ -0,0 +1,13 @@
+const mongodb = require('mongodb');
+const mongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const database = 'task-manager'
+
+mongoClient.connect(connectionURL, {useNewUrlParser: true
+}, (error, client) => {
+  if(error) {
+   return console.log('Unable to connect to database');
+  }
+})
+
